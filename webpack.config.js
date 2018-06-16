c97066fab78f918288dd41a732ecd3207549043e
Sherali <sherali.uz@gmail.com>
2018-06-16 21:23:27 +0500

    chore(webpack config): clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractPlugin = new ExtractTextPlugin("style.css");
 module.exports = {
@@ -23,10 +24,27 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ["css-loader", "sass-loader"]
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath:'img/',
+                        publicPath: 'img/'
+                    }
+                }
+                ]
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         extractPlugin,
         new HtmlWebpackPlugin({
             template: "./src/index.html",
